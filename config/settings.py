0bf17633b65a7339ac1003eae9525180e5f267e7
PeimanNavidiMoghadam <aref.django2078@gmail.com>
2025-04-19 21:29:16 +0330

    Add initial migrations for blog app

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     'accounts.apps.AccountsConfig',
-    'blog.app.BlogConfig',
+    'blog.apps.BlogConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
